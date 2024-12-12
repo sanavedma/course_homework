5bfd94cc843589126b68ac901724221c8b7b9ede
Oksana Efremova <124363708+sanavedma@users.noreply.github.com>
2024-12-12 19:34:42 +0500

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,23 +1,21 @@
-import random
-
-
 def is_prime(func):
     def wrapper(*args):
-        res = func(args)
-        if res % random.randint(1, res) == 0:
-            return 'Составное'
-        else:
+        res = func(*args)
+        print(res)
+        counter = 0
+        for i in range(2, res):
+            if res % i == 0:
+                counter += 1
+
+        if counter <= 0:
             return 'Простое'
+        else:
+            return 'Составное'
     return wrapper
 
 @is_prime
-def sum_three(*args):
-    summa = 0
-    for n in args:
-        for i in n:
-            summa += i
-    print(summa)
-    return summa
+def sum_three(a, b, c):
+    return a + b + c
 
-result = sum_three(2, 3, 6)
+result = sum_three(0, 0, 8)
 print(result)
